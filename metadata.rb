2bd5d648cb9c2955b2af1eed983b79ba70b8b6e1
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2018-10-03 16:45:57 +0200

    Release v2.2.1

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,8 @@ license 'Apache-2.0'
 description 'Installs and configures a Chef handler for sending errors to Sentry.'
 long_description 'Installs and configures a Chef handler for sending errors to Sentry.'
 
-version '2.2.0'
+version '2.2.1'
+
 chef_version '>= 12.1'
 
 supports 'ubuntu'
